f6d0cf13b210c81e1e1057326ed79e3e92af455d
MrCherry89 <abgarnati@yandex.ru>
2024-12-21 03:14:57 +0400

    ok

--- js/script.js
+++ js/script.js
@@ -3,20 +3,24 @@ $(document).ready(function () {
     minimumResultsForSearch: 6,
   });
 
-  const tabButtons = document.querySelectorAll(".tab-button");
-  const tabContents = document.querySelectorAll(".tab-content");
+  const tabGroups = document.querySelectorAll(".tabs");
 
-  tabButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const tabId = button.dataset.tab;
+  tabGroups.forEach((tabs) => {
+    const tabButtons = tabs.querySelectorAll(".tab-button");
+    const tabContents = tabs.querySelectorAll(".tab-content");
 
-      // Удаляем активные классы
-      tabButtons.forEach((btn) => btn.classList.remove("active"));
-      tabContents.forEach((content) => content.classList.remove("active"));
+    tabButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const tabId = button.dataset.tab;
 
-      // Добавляем активные классы на выбранные элементы
-      button.classList.add("active");
-      document.getElementById(tabId).classList.add("active");
+        // Удаляем активные классы только внутри текущей группы
+        tabButtons.forEach((btn) => btn.classList.remove("active"));
+        tabContents.forEach((content) => content.classList.remove("active"));
+
+        // Добавляем активные классы на выбранные элементы
+        button.classList.add("active");
+        tabs.querySelector(`#${tabId}`).classList.add("active");
+      });
     });
   });
 
@@ -38,5 +42,8 @@ $(document).ready(function () {
 
   $(function () {
     $("#slider-range").slider();
+  });
+  $(function () {
+    $("#slider-range2").slider();
   });
 });
